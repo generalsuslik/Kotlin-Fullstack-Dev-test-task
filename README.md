e13fe5cdfd23100a47e6d33941b4eb9695c35983
generalsuslik <glebrusyaev77@gmail.com>
2024-09-08 21:22:46 +0300

    minor main.cpp refactor + readme

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 make && make run && make clean
 ```
 
+[Task](https://github.com/generalsuslik/Kotlin-Fullstack-Dev-test-task/blob/master/TASK.md) 
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,6 @@ constexpr ld start_sum = 1'000'000;
 
 int main() {
 
-	//Account* account = new Account(NCurrency::RUB, start_sum);
-	
-	//printf("Yo\n");
-	//AccountInfo* info = account->get_info();
-
-	//std::cout << std::fixed << info->amount << " " << info->currency << std::endl;
-	//printf("%.2Lf %d\n", info->amount, info->currency);
-
-	//delete account;
 	Terminal* terminal = new Terminal();
 	printf("Terminal created\n");
 	User* user = new User();
